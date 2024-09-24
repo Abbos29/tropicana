5e5df410d2d1df11935ae54c3e29764bc54a592e
Abbos29 <abbosvaliev2003@gmail.com>
2024-09-24 22:00:27 +0500

    feat: new project add header + responsive

---
+++ build/js/scripts.js
@@ -0,0 +1,9 @@
+// Custom Scripts
+// Custom scripts
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.header__menu')
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('open-menu')
+})
+
