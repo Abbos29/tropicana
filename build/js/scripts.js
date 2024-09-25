c1fa0d7283c996b91d50e47fe72a9c0fde640609
Abbos29 <abbosvaliev2003@gmail.com>
2024-09-25 22:16:57 +0500

    feat: finish homepage and not finished footer

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -7,3 +7,87 @@ burger.addEventListener('click', () => {
     menu.classList.toggle('open-menu')
 })
 
+
+
+
+
+
+
+// SWIPERS
+
+
+const swiperWaysList = new Swiper('.ways-list__swiper', {
+
+    navigation: {
+        nextEl: '.ways-list__btns .list__swiper-next',
+        prevEl: '.ways-list__btns .list__swiper-prev',
+    },
+
+    slidesPerView: 2,
+    spaceBetween: 10,
+
+    breakpoints: {
+        1450: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 2.5,
+        },
+        768: {
+            slidesPerView: 2.2,
+        }
+    }
+});
+
+
+
+const swiperMentorList = new Swiper('.mentor-list__swiper', {
+
+    navigation: {
+        nextEl: '.mentor-list__btns .list__swiper-next',
+        prevEl: '.mentor-list__btns .list__swiper-prev',
+    },
+
+    slidesPerView: 2,
+    spaceBetween: 10,
+
+    breakpoints: {
+        1450: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 2.5,
+        },
+        768: {
+            slidesPerView: 2.2,
+        }
+    }
+});
+
+
+
+const swiperEventList = new Swiper('.event-list__swiper', {
+
+    navigation: {
+        nextEl: '.event-list__btns .list__swiper-next',
+        prevEl: '.event-list__btns .list__swiper-prev',
+    },
+
+    slidesPerView: 2,
+    spaceBetween: 10,
+
+    breakpoints: {
+        1450: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 2.5,
+        }
+    }
+});
